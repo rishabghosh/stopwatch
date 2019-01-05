08ef3c58166ed0696be71e3ce58ad7f031303800
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2019-01-05 10:42:45 +0530

    Added stopbutton to stop stopwatch also changed name of variables

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,23 @@ module.exports = {
         "commonjs": true,
         "es6": true
     },
+
     "extends": "eslint:recommended",
+
     "parserOptions": {
         "ecmaVersion": 2015,
         "sourceType": "module"
     },
+
+    "plugins": [ "html" ],
+    "settings": {
+        "html/report-bad-indent": "error",
+    },
+
     "rules": {
-        "indent": [ "error", 2 ],
-        "linebreak-style": [ "error", "unix" ],
-        "quotes": [ "error", "double" ],
-        "semi": [ "error", "always" ]
+        "indent": ["error", 2],
+        "linebreak-style": ["error", "unix"],
+        "quotes": ["error", "double"],
+        "semi": ["error", "always"]
     }
 };
--- src/stopwatch.js
+++ src/stopwatch.js
@@ -1,41 +1,54 @@
-let centiSeconds = 0;
-let seconds = 0;
-let minutes = 0;
-let hours = 0;
+let updateCentiSec;
+let updateTime;
+let printStopwatch;
 
-setInterval(function () {
+const start = function (status) {
+  let centiSeconds = 0;
+  let seconds = 0;
+  let minutes = 0;
+  let hours = 0;
 
-  centiSeconds++;
-  if (centiSeconds >= 99) {
-    centiSeconds = 0;
-  }
+  updateCentiSec = setInterval(function () {
+    centiSeconds = centiSeconds + 1;
+    if (centiSeconds >= 99) { centiSeconds = 0; }
+  }, 9);
 
-}, 9);
-
-
-setInterval(function () {
-  seconds++;
-
-  if (seconds >= 60) {
-    minutes++;
-    seconds = 0;
-    if (minutes >= 59) {
-      hours++;
+  updateTime = setInterval(function () {
+    seconds = seconds + 1;
+    if (seconds >= 60) {
+      minutes = minutes + 1;
+      seconds = 0;
+    }
+    if (seconds >= 60 && minutes >= 59) {
+      hours = hours + 1;
       minutes = 0;
     }
-  }
 
-}, 997);
+  }, 997);
 
+  printStopwatch = setInterval(function () {
 
-const main = function () {
-  setInterval(function () {
     let element = document.getElementById("txt");
-    element.innerHTML = hours + ":" + minutes + ":" + seconds + "." + centiSeconds;
+    element.innerHTML = hours + " : " + minutes +
+      " : " + seconds + "." + centiSeconds;
+
+    if (status === true) {
+      clearInterval(printStopwatch);
+      clearInterval(updateTime);
+      clearInterval(updateCentiSec);
+      console.log("cleared interval");
+    }
   }, 50);
 };
 
-
+const showClock = function () {
+  let element = document.getElementById("txt");
+  console.log("inside showClock");
+  element.innerHTML = "0 : 0 : 0.00";
+  clearInterval(updateCentiSec);
+  clearInterval(updateTime);
+  clearInterval(printStopwatch);
+};
 
 
 
